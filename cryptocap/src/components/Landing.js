57e2309f92fb028b2d48aebda017d809ab26ad6f
MichaelBoegner <cmikeb@gmail.com>
2019-03-09 20:01:24 -0500

    Api GET successful

--- cryptocap/src/components/Landing.js
+++ cryptocap/src/components/Landing.js
@@ -5,11 +5,13 @@ export default class Landing extends Component {
 
     componentDidMount() {
 
-        const header = {
-            'X-CoinAPI-Key': process.env.COINAPI_API,
-        }
+        const axiosConfig = {
+            headers: {
+                'X-CoinAPI-Key': '2538C11E-6337-4837-962F-F75536406B04',
+            }
+        };
 
-        axios.get('https://rest.coinapi.io/v1/exchangerate/BTC', header)
+        axios.get('https://rest.coinapi.io/v1/exchangerate/BTC', axiosConfig)
             .then(resp => {
                 console.log(resp); 
             })
